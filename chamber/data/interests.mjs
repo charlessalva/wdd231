afb88311d6c7f051c904ead2eda1b99fa7a77b15
csalva@byupathway.edu <charlesnephiibascosalva@gmail.com>
2025-11-30 20:29:03 -0700

    increasing performance

--- chamber/data/interests.mjs
+++ chamber/data/interests.mjs
@@ -2,55 +2,55 @@ export const interests = [
     {
         "name": "Kapitolyo Food District",
         "address": "Kapitolyo, Pasig City",
-        "image": "images/kapitolyo.jpeg",
+        "image": "images/kapitolyo.webp",
         "description": "A popular dining hub known for its unique restaurants and creative food concepts."
     },
     {
         "name": "Pasig Rainforest Park",
         "address": "F. Legaspi Ave., Maybunga, Pasig City",
-        "image": "images/rainforest.jpg",
+        "image": "images/rainforest.webp",
         "description": "A 10-hectare eco-park featuring a zoo, gardens, swimming pools, and adventure areas."
     },
     {
         "name": "Ortigas Center",
         "address": "Ortigas, Pasig City",
-        "image": "images/ortigas.jpg",
+        "image": "images/ortigas.webp",
         "description": "A major Metro Manila business district filled with malls, high-rises, and offices."
     },
     {
         "name": "The Grove Retail Row",
         "address": "E. Rodriguez Jr. Ave., Pasig City",
-        "image": "images/grove.jpg",
+        "image": "images/grove.webp",
         "description": "A modern lifestyle area offering cafés, wellness hubs, and restaurants."
     },
     {
         "name": "Pasig Cathedral",
         "address": "Caruncho Ave., Malinao, Pasig City",
-        "image": "images/cathedral.jpg",
+        "image": "images/cathedral.webp",
         "description": "One of the oldest churches in the Philippines, built in the 16th century."
     },
     {
         "name": "Tiendesitas",
         "address": "Ortigas Ave. Extension, Pasig City",
-        "image": "images/tiendesitas.jpg",
+        "image": "images/tiendesitas.webp",
         "description": "A family-friendly retail complex offering Filipino crafts, food stalls, and entertainment."
     },
     {
         "name": "Estancia Mall",
         "address": "Capitol Commons, Pasig City",
-        "image": "images/estancia.jpg",
+        "image": "images/estancia.webp",
         "description": "A premium shopping mall featuring curated shops, restaurants, cinemas, and events."
     },
     {
         "name": "Lopez Museum & Library",
         "address": "Benpres Building, Pasig City",
-        "image": "images/lopez.jpg",
+        "image": "images/lopez.webp",
         "description": "A cultural institution showcasing Philippine art, archives, and historical collections."
     },
     {
         "name": "Bahay na Tisa",
         "address": "P. Burgos Street, San Jose, Pasig City",
-        "image": "images/bahay-na-tisa.jpg",
+        "image": "images/bahay-na-tisa.webp",
         "description": "One of Pasig’s oldest surviving ancestral homes, famous for its clay-tile roof and preserved Spanish-era architecture."
     }
 ];
